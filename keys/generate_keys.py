6bfe8dd2abd0eb676b03e9c00faa1b35355d7b3b
Michael Brown <mbrown1413@gmail.com>
2011-10-11 11:28:14 -0400

    Create *_accepted_certs.pem when generating keys.

--- keys/generate_keys.py
+++ keys/generate_keys.py
@@ -32,25 +32,36 @@ if __name__ == '__main__':
     elif argv[1] == "proxy":
         cert_file = "proxy_cert.pem"
         key_file = "proxy_key.pem"
+        ca_certs_file = "proxy_accepted_certs.pem"
     elif argv[1] == "client":
         cert_file = "client_cert.pem"
         key_file = "client_key.pem"
+        ca_certs_file = "client_accepted_certs.pem"
     else:
         usage()
 
     current_dir = path.realpath(path.join(__file__, "../"))
     cert_file = path.join(current_dir, cert_file)
     key_file = path.join(current_dir, key_file)
+    ca_certs_file = path.join(current_dir, ca_certs_file)
 
     generate_x509(cert_file, key_file)
+
+    # Create ca_certs_file
+    if not path.exists(ca_certs_file):
+        open(ca_certs_file, 'w').close()
 
     print
     print "Cert generated:", cert_file
     print "Key generated:", key_file
-    print """
-After creating proxy and client certificates, you need to make the proxy and
-client trust eachother.  To do this, copy "proxy_cert.pem" to
-"client_accepted_certs.pem" and copy "client_cert.pem" to
-"proxy_accepted_certs.pem".
-"""
+    if argv[1] == "proxy":
+        print """
+Now you should copy your client's certificate file into %s, so the proxy will
+accept the client's connections.
+""" % ca_certs_file
+    elif argv[1] == "client":
+        print """
+Now you should copy your proxy's certificate file into %s, so the client will
+trust the proxy and be able to connect to it.
+""" % ca_certs_file
 
